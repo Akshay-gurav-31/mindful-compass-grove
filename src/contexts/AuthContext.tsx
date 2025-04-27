969b5a8ee7ac5ef88e71749431cedc056191bc69
Akshay Gurav <mathsjourneywithakshay@gmail.com>
2025-04-27 18:19:29 +0530

    Delete src directory

--- src/contexts/AuthContext.tsx
+++
@@ -1,5 +0,0 @@
-
-// This file now serves as an entry point to the auth context
-// Exporting the Auth Provider and useAuth hook
-import { AuthProvider, useAuth } from './AuthProvider';
-export { AuthProvider, useAuth };
